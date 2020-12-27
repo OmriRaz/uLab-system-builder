f23ed6bf3f8759c749899c3094615e3e888b5220
pixelol1234 <>
2020-12-27 20:00:46 +0200

    Added .v file writing, fixed issue with commas

--- FILE_V/VFileGeneration.cs
+++ FILE_V/VFileGeneration.cs
@@ -10,13 +10,43 @@ namespace uLab_system_builder
     {
         public static void GenerateVFile(MainWindow window, string path, string projectName)
         {
-            WriteGeneral(path, projectName);
+            WriteAll(window, path, projectName);
         }
-        private static void WriteGeneral(string path, string projectName)
+        private static void WriteAll(MainWindow window, string path, string projectName)
         {
             string settings = VFileWriteSettings.WRITES_MODULE + projectName + "(" + "\n\n"; // name of project
+            settings += VFileWriteSettings.WRITES_CLOCK;
+
+            if (window._4x_SwitchesBox.IsChecked == true)
+            {
+                settings += ",\n" + VFileWriteSettings.WRITES_SWITCH;
+            }
+            if(window._2x_push_buttonsBox.IsChecked == true)
+            {
+                settings += ",\n" + VFileWriteSettings.WRITES_BUTTON;
+            }
+            if (window._3x_7_SegmentBox.IsChecked == true)
+            {
+                if (window._8x_LEDsBox.IsChecked == true || window.GPIOBox.IsChecked == true) // 7seg not last
+                {
+                    settings += ",\n" + VFileWriteSettings.WRITES_SEVENSEG;
+                }
+                else
+                {
+                    settings += ",\n" + VFileWriteSettings.WRITES_SEVENSEG_NO_COMMAS;
+                }
+            }
+            if (window._8x_LEDsBox.IsChecked == true)
+            {
+                settings += ",\n" + VFileWriteSettings.WRITES_LED;
+            }
+            if (window.GPIOBox.IsChecked == true)
+            {
+                settings += ",\n" + VFileWriteSettings.WRITES_GPIO;
+            }
 
             Helper.AppendToFile(path, settings);
+            Helper.AppendToFile(path, VFileWriteSettings.WRITES_END_LINE);
         }
     }
 }
--- FILE_V/VFileWriteSettings.cs
+++ FILE_V/VFileWriteSettings.cs
@@ -9,13 +9,21 @@ namespace uLab_system_builder
     public class VFileWriteSettings
     {
         public const string WRITES_MODULE = "module ";
-        public const string WRITES_SWITCH = @"  input           [3:0]           SW,";
-        public const string WRITES_BUTTON = @"  input           [1:0]           BUTTON,";
-        public const string WRITES_SEVENSEG = 
-@"    output          [6:0]           HEX0,
-      output          [6:0]           HEX1,
-      output          [6:0]           HEX2,";
+        public const string WRITES_CLOCK = "\tinput\t\t\t\t\tCLK_50";
+        public const string WRITES_SWITCH = "\tinput\t\t[3:0]\t\tSW";
+        public const string WRITES_BUTTON = "\tinput\t\t[1:0]\t\tBUTTON";
+        public const string WRITES_SEVENSEG = "\toutput\t\t[6:0]\t\tHEX0,\n"
+    + "\toutput\t\t[6:0]\t\tHEX1,\n"
+    + "\toutput\t\t[6:0]\t\tHEX2";
+        public const string WRITES_SEVENSEG_NO_COMMAS = "\toutput\t\t[6:0]\t\tHEX0,\n"
+    + "\toutput\t\t[6:0]\t\tHEX1,\n"
+    + "\toutput\t\t[6:0]\t\tHEX2";
 
-        public const string WRITES_LED = @"  output          [7:0]           LED,";
+        public const string WRITES_LED = "\toutput\t\t[7:0]\t\tLED";
+        public const string WRITES_GPIO = "\t//inout\t\t[35:0]\t\tGPIO";
+        public const string WRITES_END_LINE = @"
+
+);
+endmodule";
     }
 }
